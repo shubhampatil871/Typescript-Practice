070fba0cb543a022203c1d58a1cb8ad67a71ce93
Shubham Patil <shubhampatil871@gmail.com>
2024-04-20 19:45:08 +0530

    initial commit

---
+++ app.ts
@@ -0,0 +1,20 @@
+// let firstNumber: number = 34;
+// let secondNumber: number = 69;
+
+function addNumber(firstNumber:number,secondNumber:number):number{
+    return firstNumber + secondNumber
+}
+
+console.log(addNumber(34,69));
+
+interface Person{
+    name:string;
+    age:number;
+}
+
+
+let john: Person = { name: "John", age: 30 };
+let shubham :Person ={ name:"shubham",age:27};
+
+console.log(shubham);
+
